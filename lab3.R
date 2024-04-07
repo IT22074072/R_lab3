ef0584b615b0aa692f2b919d52bfb35803fe7818
Dinithi Sanjana <IT22074072@my.sliit.lk>
2024-04-07 06:52:36 +0530

    Univariate Analysis(pie,bar plot, boxplot)

--- lab3.R
+++ lab3.R
@@ -16,6 +16,27 @@ fix(data)
 data$Accommodation<-factor(data$Accommodation, c(1,2,3), c("Stays at Home", "Boarded Students", "Lodging"))
 fix(data)
 
-
 #add our modifications into the imported data set
 attach(data)
+
+
+
+
+#univariate analysis
+gender.freq<-table(Gender)  #frequency tables
+acc.freq<-table(Accommodation)
+gender.freq
+acc.freq
+
+#pie chart
+pie(gender.freq,"Pie chart for gender")
+pie(acc.freq,"Pie chart for accommodation")
+
+#barplot
+barplot(gender.freq,main = "Bar plot for gender", ylab = "Frequency")
+abline(h=0)
+barplot(acc.freq, main = "Bar plot for accommodation", ylab = "Frequency")
+abline(h=0)
+
+#boxplot
+boxplot(Age,main = "Boxplot for age",ylab="Age", outpch=8)
